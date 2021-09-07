a80238e76e733431711d2155f446a9befd80e407
Rion <rionfx.42@gmail.com>
2021-09-07 18:20:39 +0900

    first commit

---
+++ CloneInstagram/CloneInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneInstagramApp.swift
+//  CloneInstagram
+//
+//  Created by FX on 2021/09/07.
+//
+
+import SwiftUI
+
+@main
+struct CloneInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
