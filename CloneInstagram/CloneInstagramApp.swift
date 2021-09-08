be020b2a05a3f3a670a8f4ae8b3a646006f05c13
Rion <rionfx.42@gmail.com>
2021-09-08 17:07:03 +0900

    Connecting SwiftUI App to Firebase

--- CloneInstagram/CloneInstagramApp.swift
+++ CloneInstagram/CloneInstagramApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
+
 
 @main
 struct CloneInstagramApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOpinions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Firebase...")
+        FirebaseApp.configure()
+        return true
     }
 }
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CloneInstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+pod 'FirebaseUI/Google'
+pod 'Firebase/Storage'
+pod 'Firebase/Firestore'
+pod 'Firebase/Auth'
+pod 'URLImage'
+  # Pods for CloneInstagram
+
+  target 'CloneInstagramTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CloneInstagramUITests' do
+    # Pods for testing
+  end
+
+end
